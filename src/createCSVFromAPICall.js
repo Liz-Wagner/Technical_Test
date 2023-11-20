46318ac1cb113e973f5ea3e7ff69af350858b332
Liz.Wagner <liz.vogner@gmail.com>
2023-11-20 16:28:07 -0700

    initial commit

---
+++ src/createCSVFromAPICall.js
@@ -0,0 +1,44 @@
+const axios = require("axios")
+const fs = require("fs").promises;
+require('dotenv').config();
+
+
+const githubUrl = process.env.Bootstrap_GitHub_API_URL
+
+//call github api and return release info
+async function fetchBootstapReleaseInfo() {
+    try {
+        const response = await axios.get(githubUrl);
+        //map over returned json data and retrieve requested data
+        return response.data.map(info => ({
+            created_at: info.created_at,
+            tag_name: info.tag_name,
+            zipball_url: info.zipball_url
+        }));
+    } catch (error) {
+        console.error("Error fetching data:", error.message)
+        return null;
+    }
+}
+
+//transform json data to csv
+async function convertJSONBootstrapDataToCSV() {
+    const releaseInfo = await fetchBootstapReleaseInfo();
+    const dataCsv = releaseInfo.reduce((acc, info) => {
+        acc += `${info.created_at}, ${info.tag_name}, ${info.zipball_url}\n`;
+        return acc;
+    },
+    `Created Date, Tag Name, Zip URL\n` //CSV column names
+    );
+
+    fs.writeFile("bootstrap_release_info.csv", dataCsv, "utf8")
+        .then(() =>console.log('Data written to bootstrap_release_info.csv'))    
+        .catch((error) => console.error('Error writing to file:', err))
+    }
+
+convertJSONBootstrapDataToCSV();
+
+module.exports = {
+    fetchBootstapReleaseInfo, 
+    convertJSONBootstrapDataToCSV
+} 
---
+++ tests/createCSV.test.js
@@ -0,0 +1,43 @@
+const axios = require("axios");
+const { fetchBootstapReleaseInfo, convertJSONBootstrapDataToCSV } = require("../src/createCSVFromAPICall");
+
+const githubUrl = process.env.Bootstrap_GitHub_API_URL
+
+describe("src/createCSVFromAPICall", () => {
+    describe("fetchBootstrapReleaseInfo()", () => {
+        const releaseData = [
+            {
+            created_at: '2020-11-11T17:07:37Z',
+            tag_name: 'v5.0.0-alpha3',     
+            zipball_url: 'https://api.github.com/repos/twbs/bootstrap/zipball/v5.0.0-alpha3'
+            },
+            {
+            created_at: '2020-06-16T19:12:01Z',  
+            tag_name: 'v5.0.0-alpha1',
+            zipball_url: 'https://api.github.com/repos/twbs/bootstrap/zipball/v5.0.0-alpha1'
+            }
+        ];
+
+        beforeEach(() => {
+            jest.spyOn(axios, 'get');
+        });
+        
+        afterEach(() => {
+            jest.clearAllMocks();
+        });
+
+        it("should make GET request to appropriate URL", async () => {
+            await fetchBootstapReleaseInfo();
+            const expectedURL = githubUrl
+            expect(axios.get).toHaveBeenCalledWith(expectedURL);
+        })
+
+        it("should resolve with an object containing release data", async () => {
+            axios.get.mockImplementation(() => Promise.resolve({ data: releaseData }));
+
+            const response = await fetchBootstapReleaseInfo();
+
+            expect(response).toEqual(releaseData)
+        });
+    })
+})
